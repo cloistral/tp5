dfdb328699b5490593a86fd24215e0947484894f
North <North@Krutoi.local>
2018-06-07 09:58:15 +0800

    tp5

--- public/index.php
+++ public/index.php
@@ -12,6 +12,9 @@
 // [ 应用入口文件 ]
 
 // 定义应用目录
-define('APP_PATH', __DIR__ . '/../application/');
+define('APP_PATH', __DIR__ . '/../app/');
+
+define('CONF_PATH',__DIR__ . '/../conf/');
+
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
